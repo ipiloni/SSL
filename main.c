f730093c1c8d5a0d0c3df2905f0bc394ecd67d5f
ipiloni <ipiloni@frba.utn.edu.ar>
2021-09-01 15:31:31 -0300

    Pusheo de prueba

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "scanner.h"
+
+int main(){
+
+    get_token();
+
+    return 0;
+}
---
+++ scanner.h
@@ -0,0 +1,6 @@
+#ifndef SCANNER_H_INCLUDED
+#define SCANNER_H_INCLUDED
+
+void get_token();
+
+#endif //SCANNER_H_INCLUDED
